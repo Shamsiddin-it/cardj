0ef04e9c0dc4af21debf31054bcbeafe2bd2246a
Shamsiddin-it <samsiddinarbobov@gmail.com>
2024-10-29 18:18:22 +0500

    problem with photos in company

--- car/models.py
+++ car/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Company(models.Model):
     name = models.CharField(max_length=50)
-    image = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=None)
+    image = models.ImageField(upload_to="static/images", height_field=None, width_field=None, max_length=None)
     def __str__(self):
         return self.name
 
